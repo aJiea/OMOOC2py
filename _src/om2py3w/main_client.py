b04c5145d6cbaf8b6d39ebbfa9815d9ac915dff2
aJiea <perso.anastasia.chen@gmail.com>
2015-11-04 20:42:54 +0800

    Create main_client.py

---
+++ _src/om2py3w/main_client.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# author: aJiea
+# a diary program
+
+
+
+import socket
+
+s = socket.socket()
+
+host = socket.gethostname()
+port = 8088
+
+s.connect((host,port))
+print s.recv(1024)
+
+
+
+import time
+from sys import exit
+
+print "欢迎来到Miss Python的秘密日记"
+psw = "1234"
+guess = str(raw_input('四位数暗号：'))
+
+if guess == psw:
+    print 'Welcome!'
+
+    print "选择题：a.回顾过往 ; b.书写现在 ; c.创造未来"
+
+    choose = raw_input('想好没~(～o￣▽￣)～o 。。')
+
+    if choose == 'a':
+       f = open("misspython.txt","r")
+       diary = f.read()
+       print(diary)
+
+
+    elif choose == 'b':
+        f = open ("misspython.txt","a")
+        diary = raw_input('>>>') 
+        f.write("\n" + time.strftime('%Y/%m/%d') + " " + diary + "\n")  
+        print diary
+        print "已保存，Adios！"
+ 
+    elif choose == 'c':
+        print "好好学习，Adios！"
+        exit()
+
+
+else:
+    print '不对，Adios！'
+    exit()
+
+
+    f.close()
