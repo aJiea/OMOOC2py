4c53d52e676d32ec6b4b0162c920b32a7bd3d769
aJiea <perso.anastasia.chen@gmail.com>
2015-11-06 17:22:35 +0800

    Create zen.py
    The Zen of Python

---
+++ _src/om2py4w/4wex0/zen.py
@@ -0,0 +1,37 @@
+# The Zen of Python by Tim Peters
+# "Translated" by aJiea
+
+def python():
+    beautiful > ugly                   # Beautiful is better than ugly.
+    explicit > implicit                # Explicit is better than implicit.
+    simple > "complex" > complicated   # Simple is better than complex. Complex is better than complicated.
+    flat > nested                      # Flat is better than nested.
+    sparse > dense                     # Sparse is better than dense.
+    readable = good				       # Readability counts.
+    rules > special cases 			   # Special cases aren't special enough to break the rules.
+    practicality > purity			   # Although practicality beats purity.
+
+# Errors should never pass silently.
+# Unless explicitly silenced.
+if(error > 0):    					   
+    print "Check error"
+if(error == explicitly_silenced):       
+    print "OK"		
+
+# In the face of ambiguity, refuse the temptation to guess.
+# There should be one-- and preferably only one --obvious way to do it.
+# Although that way may not be obvious at first unless you're Dutch.
+name = raw_imput()					       
+Dutch = Guido  								   
+
+if(name!=Dutch):  									   
+    solution = 1 and solution != guess
+
+    now > never 					   # Now is better than never.
+    never > perfect  	    	 	   # Although never is often better than *right* now.
+    hard = bad						   # If the implementation is hard to explain, it's a bad idea.
+    easy = good 					   # If the implementation is easy to explain, it may be a good idea.
+    namespace = good * 2               # Namespaces are one honking great idea 
+
+print Python     					   # -- let's do more of those
+
